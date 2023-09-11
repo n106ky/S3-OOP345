e40864a7d4ebdbe46119e2fb05dbfadb7836e912
n106ky <n106ky@gmail.com>
2023-09-11 16:09:38 -0400

    WS01 Part2 HeapBugs

---
+++ WS01/PART2/RideRequest.cpp
@@ -0,0 +1,70 @@
+/*
+   OOP345 NFF WS01
+   PROF. HONG HUANG
+
+   KA YING, CHAN #123231227
+   kchan151@myseneca.ca
+
+   I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+   Completed on 2023 SEP 11
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include "RideRequest.h"
+using namespace std;
+
+double g_discount;
+double g_taxrate;
+static int counter = 1;
+
+namespace sdds {
+   RideRequest::RideRequest() {}
+   void RideRequest::read(istream& is) {
+      char tempDiscount{};
+      char tempRideDesc[100]{};
+      if (is.good()) {
+         is.getline(customerName, CUST_NAME_SIZE, ',');
+         is.getline(tempRideDesc, 100, ',');
+         is >> priceOfRide;
+         is.ignore();
+         is >> tempDiscount;
+      }
+      if (rideDesc) {
+         delete[] rideDesc;
+      }
+      rideDesc = new char[strlen(tempRideDesc) + 1];
+      strcpy(rideDesc, tempRideDesc);
+
+      if (tempDiscount == 'Y') {
+         isDiscounted = true;
+      }
+      else {
+         isDiscounted = false;
+      }
+   }
+   void RideRequest::display() {
+      cout
+         << left << setw(2) << counter++ << ". ";
+      if (customerName[0] != '\0') {
+         cout
+            << left << setw(10) << customerName << "|"
+            << left << setw(25) << rideDesc << "|"
+            << fixed << setprecision(2) << setw(12) << priceOfRide * (1 + g_taxrate) << "|";
+         if (isDiscounted) {
+            cout << right << setw(13) << priceOfRide * (1 + g_taxrate) - g_discount << endl;
+         }
+         else {
+            cout << endl;
+         }
+      }
+      else {
+         cout << "No Ride Request" << endl;
+      }
+   }
+   RideRequest::~RideRequest() {
+      delete[] rideDesc;
+   }
+}
---
+++ WS01/PART2/RideRequest.h
@@ -0,0 +1,35 @@
+#ifndef SDDS_RIDEREQUEST_H
+#define SDDS_RIDEREQUEST_H
+
+/*
+   OOP345 NFF WS01
+   PROF. HONG HUANG
+
+   KA YING, CHAN #123231227
+   kchan151@myseneca.ca
+
+   I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+   Completed on 2023 SEP 11
+*/
+extern double g_discount; // HAS TO DEFINE IT OUTSIDE SDDS
+extern double g_taxrate;
+
+namespace sdds {
+
+   const int CUST_NAME_SIZE = 10;   // Included NULL terminator
+   // const int RIDE_DESC_SIZE = 25;   // Included NULL terminator
+
+   class RideRequest {
+      char customerName[CUST_NAME_SIZE]{};
+      char* rideDesc{ nullptr };
+      double priceOfRide{};         // Not yet taxed
+      bool isDiscounted{};
+   public:
+      RideRequest();
+      void read(std::istream& is);
+      void display();
+      ~RideRequest();
+   };
+}
+
+#endif
